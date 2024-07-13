f0a7258b4307b0513d066fd73c54b74b691e7036
Shelton Ngwenya <76778791+sheltsnn15@users.noreply.github.com>
2024-07-13 23:40:07 +0100

    Update setup_dev_env.sh

--- linux/setup_dev_env.sh
+++ linux/setup_dev_env.sh
@@ -15,7 +15,7 @@ install_essentials() {
     echo "Updating package list and installing essential packages..."
     sudo apt-get update
 
-    packages=("build-essential" "vim-nox" "valgrind" "git" "gh" "zip" "unzip" "lazygit" "tmux" "xclip")
+    packages=("build-essential" "vim-nox" "valgrind" "git" "gh" "zip" "unzip" "lazygit" "tmux" "xclip" "ca-certificates" "curl" "ninja-build" "gettext" "libtool" "libtool-bin" "autoconf" "automake" "cmake" "g++" "pkg-config" "unzip")
     for pkg in "${packages[@]}"; do
         if ! dpkg -l | grep -q $pkg; then
             sudo apt-get install -y $pkg
@@ -32,8 +32,7 @@ install_go() {
         wget https://go.dev/dl/go1.20.6.linux-amd64.tar.gz -O go.tar.gz
         sudo tar -C /usr/local -xzf go.tar.gz
         rm go.tar.gz
-        echo "export PATH=$PATH:/usr/local/go/bin" >>~/.bashrc
-        source ~/.bashrc
+        echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.bashrc
     else
         echo "Go is already installed."
     fi
@@ -44,7 +43,7 @@ install_gvm() {
     if [ ! -d "$HOME/.gvm" ]; then
         echo "Installing GVM..."
         bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-        source ~/.gvm/scripts/gvm
+        echo 'source ~/.gvm/scripts/gvm' >>~/.bashrc
     else
         echo "GVM is already installed."
     fi
@@ -67,7 +66,7 @@ install_rust() {
     if ! command -v rustc &>/dev/null; then
         echo "Installing Rust..."
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-        source $HOME/.cargo/env
+        echo 'source $HOME/.cargo/env' >>~/.bashrc
     else
         echo "Rust is already installed."
     fi
@@ -80,8 +79,7 @@ install_miniconda() {
         wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
         bash miniconda.sh -b -p $HOME/miniconda
         rm miniconda.sh
-        echo "export PATH=$HOME/miniconda/bin:$PATH" >>~/.bashrc
-        source ~/.bashrc
+        echo 'export PATH=$HOME/miniconda/bin:$PATH' >>~/.bashrc
     else
         echo "Miniconda is already installed."
     fi
@@ -92,7 +90,7 @@ install_sdkman() {
     if [ ! -d "$HOME/.sdkman" ]; then
         echo "Installing SDKMAN..."
         curl -s "https://get.sdkman.io" | bash
-        source "$HOME/.sdkman/bin/sdkman-init.sh"
+        echo 'source "$HOME/.sdkman/bin/sdkman-init.sh"' >>~/.bashrc
     else
         echo "SDKMAN is already installed."
     fi
@@ -101,9 +99,6 @@ install_sdkman() {
 # Function to build and install Neovim from source
 install_neovim() {
     if ! command -v nvim &>/dev/null; then
-        echo "Installing dependencies for Neovim..."
-        sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
-
         echo "Cloning Neovim repository..."
         git clone https://github.com/neovim/neovim.git --depth=1
         mkdir -p ~/Documents/Packages/
@@ -156,7 +151,6 @@ install_fzf_tab_completion() {
         echo "Installing fzf-tab-completion..."
         git clone https://github.com/lincheney/fzf-tab-completion.git "$FZF_TAB_DIR/fzf-tab-completion"
         echo "source $FZF_TAB_DIR/fzf-tab-completion/bash" >>~/.bashrc
-        source ~/.bashrc
     else
         echo "fzf-tab-completion is already installed."
     fi
@@ -174,40 +168,54 @@ install_tpm() {
 
 # Function to install lazygit
 install_lazygit() {
-    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-    tar xf lazygit.tar.gz lazygit
-    sudo install lazygit /usr/local/bin
+    if ! command -v lazygit &>/dev/null; then
+        echo "Installing lazygit..."
+        LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+        curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+        tar xf lazygit.tar.gz lazygit
+        sudo install lazygit /usr/local/bin
+        rm lazygit.tar.gz lazygit
+    else
+        echo "lazygit is already installed."
+    fi
 }
 
 # Function to install lazydocker
 install_lazydocker() {
-    curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+    if ! command -v lazydocker &>/dev/null; then
+        echo "Installing lazydocker..."
+        curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+    else
+        echo "lazydocker is already installed."
+    fi
 }
 
 # Function to install Docker Engine
 install_docker() {
-    # Set up Docker's apt repository
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    if ! command -v docker &>/dev/null; then
+        echo "Installing Docker..."
+        # Set up Docker's apt repository
+        sudo apt-get update
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-    # Add the repository to Apt sources
-    echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-    sudo apt-get update
+        # Add the repository to Apt sources
+        echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+            sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+        sudo apt-get update
 
-    # Install Docker packages
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo usermod -aG docker $USER
+        # Install Docker packages
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        sudo usermod -aG docker $USER
+    else
+        echo "Docker is already installed."
+    fi
 }
 
 # Run all the functions
-# install_oh_my_bash
 install_essentials
 install_go
 install_gvm
@@ -223,5 +231,8 @@ install_tpm
 install_lazygit
 install_lazydocker
 install_docker
+
+# Re-source .bashrc to apply changes
+source ~/.bashrc
 
 echo "All tools installed successfully!"
