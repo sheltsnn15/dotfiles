93c6e162dbd8b5a70f98157b02b25d0de841402e
Shelton Ngwenya <76778791+sheltsnn15@users.noreply.github.com>
2024-10-25 16:54:34 +0100

    Update setup_desktop_env.sh

--- linux/setup_desktop_env.sh
+++ linux/setup_desktop_env.sh
@@ -19,8 +19,6 @@ update_system() {
 configure_xrdp() {
 	log "Configuring XRDP..."
 	sudo apt-get install -y xrdp
-	sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak || true
-	sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
 	sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
 	sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
 }
@@ -28,16 +26,7 @@ configure_xrdp() {
 # Function to install additional packages for XFCE4+i3
 install_xfce4_i3_packages() {
 	log "Installing additional packages for XFCE4+i3..."
-	sudo apt-get install -y lightdm xubuntu arc-theme papirus-icon-theme gvfs gvfs-backends i3* dmenu suckless-tools feh lxappearance breeze-icon-theme picom arc-theme papirus-icon-theme
-}
-
-# Function to apply EndeavourOS theming for XFCE4
-apply_endeavouros_xfce4_theming() {
-	log "Applying EndeavourOS theming for XFCE4..."
-	git clone https://github.com/endeavouros-team/endeavouros-xfce4-theming ~/Documents/Packages/endeavouros-xfce4-theming
-	cd ~/Documents/Packages/endeavouros-xfce4-theming/etc/skel/
-	rm -rf ~/.config/xfce4 ~/.cache
-	cp -R .config/ ~/
+	sudo apt-get install -y xubuntu arc-theme papirus-icon-theme gvfs gvfs-backends i3* dmenu suckless-tools feh breeze-icon-theme picom arc-theme papirus-icon-theme
 }
 
 # Function to set up i3 as the window manager
@@ -67,36 +56,10 @@ install_snap_packages() {
 	sudo snap install obsidian --classic
 }
 
-# Function to set up Git configuration
-setup_git() {
-	log "Setting up Git global configuration..."
-	git config --global user.name "Shelton Ngwenya"
-	git config --global user.email "shelt15.nn@gmail.com"
-}
-
-# Function to generate SSH key and add it to GitHub using the GitHub CLI
-setup_ssh_key_for_github() {
-	log "Generating SSH key for GitHub..."
-	ssh-keygen -t ed25519 -C "shelt15.nn@gmail.com" -f ~/.ssh/id_ed25519 -N ""
-	eval "$(ssh-agent -s)"
-	ssh-add ~/.ssh/id_ed25519
-	log "SSH key generated."
-
-	log "Adding SSH key to GitHub using gh CLI..."
-	if ! command -v gh &>/dev/null; then
-		log "GitHub CLI (gh) is not installed. Installing..."
-		sudo apt-get install -y gh
-	fi
-	gh auth login
-	gh ssh-key add ~/.ssh/id_ed25519.pub --title "My SSH Key"
-	log "SSH key added to GitHub."
-}
-
 # Run all functions in order
 update_system
 # configure_xrdp
 install_xfce4_i3_packages
-apply_endeavouros_xfce4_theming
 setup_i3wm
 install_snap_packages
 
